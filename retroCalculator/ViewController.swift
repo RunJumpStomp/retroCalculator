a1a00b4fc776cdd21e704d98e8ee73647591a8e2
William Fairchild <williamfairchild@Williams-MacBook-Pro.local>
2016-05-18 17:34:17 -0400

    Added a sound to the button presses, and set up the label to update when integers are added to runningNumber

--- retroCalculator/ViewController.swift
+++ retroCalculator/ViewController.swift
@@ -7,19 +7,67 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    //Global Variables
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Equals = "="
+        case Empty = "Empty"
+    }
+    
+    var runningNumber = ""
+    var leftValStr = ""
+    var rightValStr = ""
+    var currentOperation: Operation = Operation.Empty
+    
+    //Outlets
+    
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    var btnSound: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        let soundUrl = NSURL(fileURLWithPath: path!)
+        do{
+            try btnSound = AVAudioPlayer(contentsOfURL: soundUrl)
+            btnSound.prepareToPlay()
+        } catch  let err as NSError{
+            print(err.debugDescription)
+        }
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //Actions
+    
+    @IBAction func buttonPressed(btn: UIButton) {
+        btnSound.play()
+        runningNumber += "\(btn.tag)"
+        outputLbl.text = runningNumber
+    }
+    
+    @IBAction func onDividePressed(sender: UIButton) {
+    }
+    
+    @IBAction func onMultiplyPressed(sender: UIButton) {
+    }
+    
+    @IBAction func onSubtractionPressed(sender: UIButton) {
+    }
+    
+    @IBAction func onAddPressed(sender: UIButton) {
     }
 
-
+    @IBAction func onEqualPressed(sender: UIButton) {
+    }
 }
 
